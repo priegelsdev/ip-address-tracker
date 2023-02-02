0e57b539490c39b5e130b3dd9bb23504f8446fc7
priegelsdev <priegelsdev@gmail.com>
2023-02-02 11:10:06 +0100

    initialize files

---
+++ index.js
@@ -0,0 +1,13 @@
+// https://geo.ipify.org/
+// https://leafletjs.com/
+
+/* Your users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location */
+
+/* - [API Key best practices from Google Developers](https://developers.google.com/maps/api-key-best-practices)
+- [How to use Mapbox securely](https://docs.mapbox.com/help/troubleshooting/how-to-use-mapbox-securely/)
+*/ 
