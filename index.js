bda72ee47f76d5f403fea1a2f210f00498b48f61
priegelsdev <priegelsdev@gmail.com>
2023-02-02 13:06:56 +0100

    write logic to get users IP address, get coordinates from geo ipify API and display map of that location

--- index.js
+++ index.js
@@ -11,3 +11,42 @@
 /* - [API Key best practices from Google Developers](https://developers.google.com/maps/api-key-best-practices)
 - [How to use Mapbox securely](https://docs.mapbox.com/help/troubleshooting/how-to-use-mapbox-securely/)
 */ 
+
+// initial function block to get user IP address and get coordinates for that IP; 
+// can be changed later on once user makes request 
+
+async function getIpAddress() {
+  try {
+    const res = await fetch('https://api.ipify.org?format=json')
+    const data = await res.json()
+    return data.ip
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// API call to get coordinates to be displayed by leaflet map
+
+async function fetchGeo(ip) {
+  const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_I6W8LXRSmni2Er5pShOMcWjbRW4dI&ipAddress=${ip}`)
+  const data = await res.json()
+  console.log({lat: data.location.lat, lng: data.location.lng})
+
+  let map = L.map('map').setView([data.location.lat, data.location.lng], 16);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+}
+
+async function fetchDetails() {
+  const ip = await getIpAddress();
+  fetchGeo(ip)
+}
+
+// call function initially to render out user IP location
+
+fetchDetails()
+
+
+
